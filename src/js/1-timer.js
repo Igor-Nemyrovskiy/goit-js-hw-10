7e3aa4310c4e6e499d1b28840db13d7f3f1a43ea
IhorN <igor.nemirovskij@gmail.com>
2026-07-17 14:10:35 +0300

    update js.timer

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -6,6 +6,7 @@ import "izitoast/dist/css/iziToast.min.css";
 
 const dateInput = document.querySelector("#datetime-picker");
 const startButton = document.querySelector("[data-start]");
+startButton.disabled = true;
 const daysElement = document.querySelector("[data-days]");
 const hoursElement = document.querySelector("[data-hours]");
 const minutesElement = document.querySelector("[data-minutes]");
